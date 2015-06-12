ef029a37184bf68a7dacaf2dab76341070928431
Yung <kuo.yung@gmail.com>
2015-06-11 17:38:18 -0700

    ?

--- polarization1.2.py
+++ polarization1.2.py
@@ -9,19 +9,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import libtiff
 import os
-from scipy.optimize import leastsq
-#from scipy.optimize import curve_fit
+#from scipy.optimize import leastsq
+from scipy.optimize import curve_fit
 from sub import extract
 
-filePath = 'E:/QCSE data/031215 polarization/run4/'
+
+filePath = 'E:/polarization data/150511/GB on glass/2'
 #filePath = '/Users/yung/run4/'
 differential_image = 0 # 1 = yes, display differential image; else = no, display mean image
-deg_0 = 167
+deg_0 = 0
 scan = 3
 displacement = -6
-n1 = 1.5
-n2 = 1.000
 plot_nmlcosinewave = 0
+last_ang = 180
+step = 5
 # 1 = normalize emission intensity by deviding the intensity in one view by the sum intensity in two views 
 # (requires: particle 0,2,4,6 in one view to be the same particle as particle 1,3,5,7 in the other view )
 # else = not normalize, plot the raw intensity reading
@@ -33,14 +34,15 @@ offint = []
 offstd = []
 BGint = []
 BGstd = []
+angle = np.arange(0, last_ang*2+1, step*2)
+
 for file in os.listdir(filePath):
     current_file = os.path.join(filePath, file)
-    S = file.split('.')   
     mov = libtiff.TiffFile(current_file)
     mov = np.array(mov.get_tiff_array()[:,:,:], dtype='d')
-    angle = np.append(angle, [int(s) for s in S if s.isdigit()])
+    
 
-    if np.mean(angle, dtype = 'int') == deg_0:
+    if file == '2_00.tif':
 
         frame = len(mov[:,0,0])
         row = len(mov[0,:,0])
@@ -59,28 +61,15 @@ for file in os.listdir(filePath):
      
         else: 
             mov_mean = np.mean(mov, axis=0)
-            
             fig, ax = plt.subplots()
-            ax.imshow(mov_mean)
+            ax.imshow(np.log(mov_mean))
             
-        #pts = np.array(plt.ginput(n=0, timeout=0))
+        pts = np.array(plt.ginput(n=0, timeout=0))
 
-        pts = np.array([[ 108.42546584,  212.76708075],
-                        [ 109.41304348,  472.52732919],
-
-                        [ 110.4068323,   206.79813665],
-                        [ 111.40062112,  466.00080745],
-
-                        [ 175.00310559,  132.27639752],
-                        [ 184.94720497,  386.68012422],
-
-
-                        [ 183.45341615,  148.17080745],
-                        [ 192.90372671,  403.58074534],
-  
-
-               ])
-
+        #pts = np.array([[ 330.95551983,  197.53644159],
+         #                [ 326.8397642,   380.68756699],
+          #               [ 280.88049303,  199.5943194 ],
+           #              [ 281.5664523,   384.11736334]])
 
         print pts
         ax.plot(pts[:,0],pts[:,1],'r+', markersize=10)
@@ -103,19 +92,19 @@ for file in os.listdir(filePath):
 
 
     blinkon_int, blinkon_std = extract.blinkon_mean(mov, pts, scan, fig, ax) 
-    blinkoff_int, blinkoff_std = extract.blinkoff_mean(mov, pts, scan, fig, ax)
-    BG_int, BG_std = extract.bg_mean(mov, pts, scan, displacement, fig, ax)
+    #blinkoff_int, blinkoff_std = extract.blinkoff_mean(mov, pts, scan, fig, ax)
+    #BG_int, BG_std = extract.bg_mean(mov, pts, scan, displacement, fig, ax)
     QDint = np.append(QDint, blinkon_int)
     QDstd = np.append(QDstd, blinkon_std)
-    offint = np.append(offint, blinkoff_int)
-    offstd = np.append(offstd, blinkoff_std)
+    #offint = np.append(offint, blinkoff_int)
+    #offstd = np.append(offstd, blinkoff_std)
     #BGint = np.append(BGint, BG_int)
     #BGstd = np.append(BGstd, BG_std)
 
 
 QDint = np.reshape(QDint, [len(angle), len(pts)])
 QDstd = np.reshape(QDstd, [len(angle), len(pts)])
-offint = np.reshape(offint, [len(angle), len(pts)])
+#offint = np.reshape(offint, [len(angle), len(pts)])
 #BGint = np.reshape(BGint, [len(angle), len(pts)])
 #BGstd = np.reshape(BGstd, [len(angle), len(pts)])
 #QDint = QDint-BGint
@@ -165,8 +154,8 @@ arti_signal = np.reshape(np.tile(np.cos(np.pi*(angle-deg_0)*2/180)**2, [len(pts)
 QDint = QDint + arti_signal*0
 for i in range(len(pts)):
     acolor = next(color)    
-    ax[i].scatter((angle-deg_0)*2, QDint[:,i], color=acolor, marker='o')
-    ax[i].errorbar((angle-deg_0)*2, QDint[:,i], xerr=None, yerr=QDstd[:,i], ecolor=acolor, fmt='none')    
+    ax[i].scatter((angle-deg_0), QDint[:,i], color=acolor, marker='o')
+    ax[i].errorbar((angle-deg_0), QDint[:,i], xerr=None, yerr=QDstd[:,i], ecolor=acolor, fmt='none')    
     #ax[i,1].scatter((angle-deg_0)*2, BGint[:,i], color=acolor, marker='^')
     #ax[i,1].errorbar((angle-deg_0)*2, BGint[:,i], xerr=None, yerr=BGstd[:,i], ecolor=acolor, fmt='none')     
     
@@ -175,20 +164,22 @@ for i in range(len(pts)):
     guess_amplitude = np.max(QDint[:,i])-np.min(QDint[:,i])
     guess_phase = (angle[int(np.where(QDint==QDint[:,i].max())[0])]-deg_0)*2*np.pi/180
     guess = [guess_amplitude, guess_phase, guess_min]
-    data_first_guess = cos_sqr(np.linspace(0,np.pi*2,1000), *guess)
+    data_first_guess = cos_sqr(np.linspace(0,last_ang*2*np.pi/180,1000), *guess)
 
-    '''
-    Non-linear least square fit
+
+
+    #Non-linear least square fit
     
-    fit_prmt, pcov = curve_fit(cos_sqr, (angle-deg_0)*2*np.pi/180, QDint[:,i], p0=guess)    
-    data_fit = cos_sqr(np.linspace(0,np.pi*2,1000), *fit_prmt)
+    fit_prmt, pcov = curve_fit(cos_sqr, (angle-deg_0)*np.pi/180, QDint[:,i], p0=guess)    
+    data_fit = cos_sqr(np.linspace(0,last_ang*2*np.pi/180,1000), *fit_prmt)
     perr = np.sqrt(np.diag(pcov))
-    '''
+    
     
     
     '''
     Least square fit
-    '''
+
+
     optimize_func = lambda x: x[0]*np.cos(x[1]-np.pi*(angle-deg_0)*2/180)**2 + x[2] - QDint[:,i]
     est_amplitude, est_phase, est_min = leastsq(optimize_func, guess)[0]
     
@@ -198,12 +189,12 @@ for i in range(len(pts)):
     A = np.append(A, est_amplitude)
     phi = np.append(phi, est_phase)
     b = np.append(b,est_min)    
+    '''
     
-    
-    ax[i].plot(np.linspace(0,360,1000), data_fit, color=acolor, linestyle='-', label='QD{}'.format(i)+' fit')
-    ax[i].plot(np.linspace(0,360,1000), data_first_guess, color=acolor, linestyle='--', label='first guess')
-    ax[i].plot(np.linspace(0,360,1000), np.tile(np.mean(QDint[:,i]), 1000), '0.75')#, label='mean')
-    ax[i].set_xlim((angle.min()-deg_0)*2-1,(angle.max()-deg_0)*2+1)    
+    ax[i].plot(np.linspace(0,last_ang*2,1000), data_fit, color=acolor, linestyle='-', label='QD{}'.format(i)+' fit')
+    #ax[i].plot(np.linspace(0,720,1000), data_first_guess, color=acolor, linestyle='--', label='first guess')
+    ax[i].plot(np.linspace(0,last_ang*2,1000), np.tile(np.mean(QDint[:,i]), 1000), '0.75')#, label='mean')
+    ax[i].set_xlim((angle.min()-deg_0)-1,(angle.max()-deg_0)+1)    
     ax[i].legend(fontsize='xx-small',frameon=None)
     fig.canvas.draw()
 
